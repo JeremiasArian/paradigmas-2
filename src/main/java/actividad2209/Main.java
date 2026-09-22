03c8022d6905b789b09a48a003cb49c612f4d854
JeremiasArian <159722626+JeremiasArian@users.noreply.github.com>
2026-09-22 16:58:10 -0300

    feat: agregar código básico en Main.java

--- src/main/java/actividad2209/Main.java
+++ src/main/java/actividad2209/Main.java
@@ -1,7 +1,46 @@
 package actividad2209;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
+        System.out.println("Bienvenido, administrador.");
+        System.out.println("Opciones disponibles:");
+        System.out.println("1) Agregar alumno");
+        System.out.println("-1 Salir de este programa.");
 
+        Scanner lector;
+        boolean continuar = true;
+
+        int opcion = 0;
+
+        while (opcion==0){
+            lector = new Scanner(System.in);
+            try {
+                opcion = lector.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada invalida");
+            }
+
+            if(opcion ==-1) {
+                break;
+            } else if(opcion == 1){
+                registrarAlumno();
+            } else{
+                System.out.println("Opciones disponibles:");
+                System.out.println("1) Agregar alumno");
+                System.out.println("-1 Salir de este programa.");
+                opcion = 0;
+            }
+        }
+    }
+
+    private static void registrarAlumno(){
+        Alumno alumno = new Alumno();
+        System.out.println("Ingrese el turno al que asiste");
+        System.out.println("1) Mañana");
+        System.out.println("2) Tarde");
+        System.out.println("3) A los dos");
     }
 }
