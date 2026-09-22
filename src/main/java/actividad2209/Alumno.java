8435c3c716115b1b3e3f3efb934b17d94081eca5
JeremiasArian <159722626+JeremiasArian@users.noreply.github.com>
2026-09-22 15:27:35 -0300

    feat: agregar fields y getters/setters a Alumno.java

--- src/main/java/actividad2209/Alumno.java
+++ src/main/java/actividad2209/Alumno.java
@@ -1,4 +1,28 @@
 package actividad2209;
 
 public class Alumno {
+    private boolean accesoAlcomedor = false;
+    private boolean poseeHermanoInscripto = false;
+
+    public boolean isAccesoAlcomedor() {
+        return accesoAlcomedor;
+    }
+
+    public void setAccesoAlcomedor(boolean accesoAlcomedor) {
+        this.accesoAlcomedor = accesoAlcomedor;
+    }
+
+    public boolean isPoseeHermanoInscripto() {
+        return poseeHermanoInscripto;
+    }
+
+    public void setPoseeHermanoInscripto(boolean poseeHermanoInscripto) {
+        this.poseeHermanoInscripto = poseeHermanoInscripto;
+    }
+
+    public enum Turno{
+        MANANA,
+        TARDE,
+        DOBLE
+    }
 }
