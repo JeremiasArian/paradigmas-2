17f5f6ce814911d1fe75eddf7ebee49da1d62763
JeremiasArian <159722626+JeremiasArian@users.noreply.github.com>
2026-09-23 13:51:25 -0300

    feat: permitir asignar la edad del alumno

--- src/main/java/actividad2209/Alumno.java
+++ src/main/java/actividad2209/Alumno.java
@@ -4,6 +4,9 @@ public class Alumno {
     private Turno turno;
     private boolean accesoAlcomedor = false;
     private boolean poseeHermanoInscripto = false;
+    private int edad = 0;
+    private boolean meses = false;
+    private boolean anios = false;
 
     public Alumno(){}
 
@@ -29,6 +32,30 @@ public class Alumno {
 
     public void setTurno(Turno turno) {
         this.turno = turno;
+    }
+
+    public boolean isMeses() {
+        return meses;
+    }
+
+    public void setMeses(boolean meses) {
+        this.meses = meses;
+    }
+
+    public boolean isAnios() {
+        return anios;
+    }
+
+    public void setAnios(boolean anios) {
+        this.anios = anios;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
     public enum Turno{
--- src/main/java/actividad2209/Main.java
+++ src/main/java/actividad2209/Main.java
@@ -71,9 +71,77 @@ public class Main {
         return a;
     }
 
+    private static void asignarEdad(Alumno a){
+        int opcion = -2;
+        while (true){
+            Scanner s = new Scanner(System.in);
+            try {
+                opcion = s.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada invalida");
+            }
+
+            if(opcion ==-1) {
+                System.out.println("Saliendo...");
+                break;
+            } else if(opcion == 1) {
+                opcion = 0;
+                System.out.println("Cuantos meses tiene el nene?");
+                int edad = -2;
+                while (true) {
+                    Scanner s1 = new Scanner(System.in);
+                    try {
+                        edad = s1.nextInt();
+                    } catch (InputMismatchException e) {
+                        System.out.println("Entrada invalida");
+                    }
+
+                    if (edad < 0 || edad > 11) {
+                        System.out.println("Sólo se permiten entre 0 a 11 meses");
+                    } else {
+                        a.setEdad(edad);
+                        a.setMeses(true);
+                        break;
+                    }
+                }
+                break;
+            }else if(opcion==2) {
+                opcion = 0;
+                System.out.println("Cuantos años tiene el nene?");
+                int edad = -2;
+                while (true) {
+                    Scanner s1 = new Scanner(System.in);
+                    try {
+                        edad = s1.nextInt();
+                    } catch (InputMismatchException e) {
+                        System.out.println("Entrada invalida");
+                    }
+
+                    if (edad < 1 || edad > 3) {
+                        System.out.println("Sólo se permiten entre 1 a 3 años");
+                    } else {
+                        a.setEdad(edad);
+                        a.setMeses(true);
+                        break;
+                    }
+                }
+                break;
+            } else{
+                System.out.println("Opciones disponibles:");
+                System.out.println("1) Para ingresar la edad en meses");
+                System.out.println("2) Para ingresar la edad en años");;
+                opcion = 0;
+            }
+        }
+    }
+
     private static void registrarAlumno(){
         Alumno alumno = new Alumno();
-        System.out.println("Ingrese el turno al que asiste");
+        System.out.println("Primero, Ingrese la edad del nene");
+        System.out.println("1) Para ingresar la edad en meses");
+        System.out.println("2) Para ingresar la edad en años");
+        asignarEdad(alumno);
+        System.out.println("Ahora, Ingrese el turno al que asistirá");
         System.out.println("1) Mañana");
         System.out.println("2) Tarde");
         System.out.println("3) A los dos");
