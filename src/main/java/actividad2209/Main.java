6c75f15e379a120c3b5792140e90ee3c6364620a
JeremiasArian <159722626+JeremiasArian@users.noreply.github.com>
2026-09-23 13:09:08 -0300

    feat: permitir asignar el turno del alumno
    Usando la técnica de method overloading

--- src/main/java/actividad2209/Main.java
+++ src/main/java/actividad2209/Main.java
@@ -38,11 +38,45 @@ public class Main {
         }
     }
 
+    private static Alumno ingresarInputYValidar(Alumno a){
+        int opcion = 0;
+        while (opcion==0){
+            Scanner s = new Scanner(System.in);
+            try {
+                opcion = s.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada invalida");
+            }
+
+            if(opcion ==-1) {
+                System.out.println("Saliendo...");
+                break;
+            } else if(opcion == 1) {
+                opcion = 0;
+                a.setTurno(Alumno.Turno.MANANA);
+            }else if(opcion==2) {
+                opcion = 0;
+                a.setTurno(Alumno.Turno.TARDE);
+            }else if(opcion==3) {
+                opcion = 0;
+                a.setTurno(Alumno.Turno.DOBLE);
+            } else{
+                System.out.println("Opciones disponibles:");
+                System.out.println("1) Mañana");
+                System.out.println("2) Tarde");
+                System.out.println("3) A los dos");
+                opcion = 0;
+            }
+        }
+        return a;
+    }
+
     private static void registrarAlumno(){
         Alumno alumno = new Alumno();
         System.out.println("Ingrese el turno al que asiste");
         System.out.println("1) Mañana");
         System.out.println("2) Tarde");
         System.out.println("3) A los dos");
+        ingresarInputYValidar(alumno);
     }
 }
